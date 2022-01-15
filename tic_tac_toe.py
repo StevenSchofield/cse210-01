d315f61477016b6733e3e4eceb7745ac3aeee232
StevenSchofield <sch21093@byui.edu>
2022-01-14 17:36:45 -0800

    Create tic_tac_toe.py
    Moved the file to another repository to fit with the assignment specifications. Presently, the program can accurately display the current board state, and can handle player input.

---
+++ tic_tac_toe.py
@@ -0,0 +1,64 @@
+# Nested array to keep track of the current board state
+game_board = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]]
+
+# Simplify the translation process so I can use numbers in code and letters in display
+piece_translation = [" ", "X", "O"]
+index_translation = ["A", "B", "C"]
+
+# Display the current board state, including indexes
+def print_board(boardArray):
+    print(" |1|2|3")
+    for rowIndex in range(0,3):
+        print_line = str(index_translation[rowIndex])
+        print("--------")
+
+        for index in boardArray[rowIndex]:
+            print_line += "|"
+            print_line += piece_translation[index]
+        
+        print(print_line)
+
+# Function to handle and verify player input
+def player_input(playerNumber, boardArray):
+    positionIndex = []
+    positionString = input("Player " + str(playerNumber) + ", please input which position you would like to mark: ")
+
+    # A position index should only have 2 characters: letter for the row, number for the colum
+    if len(positionString) != 2:
+        print("\nSorry, that input is the wrong size. Please input a position in the format of \"A1\" or \"B3\".")
+        return player_input(playerNumber)
+
+    positionIndex.append(positionString[0])
+    positionIndex.append(positionString[1])
+
+    # The first character (letter) represents the row, and should be translated to a number
+    if type(positionIndex[0]) == type(""):
+        for i in range(0, 3):
+            if index_translation[i] == positionIndex[0].capitalize():
+                positionIndex[0] = i
+                break
+            
+            if i == 2:
+                print("\nSorry, the first character needs to be one of the letters \"A\", \"B\", or \"C\". " +\
+                    "Please try again.")
+                return player_input(playerNumber)
+    else:
+        print("\nSorry, the first character needs to be one of the letters \"A\", \"B\", or \"C\". " +\
+            "Please try again.")
+        return player_input(playerNumber)
+
+    # The second character needs to be a number less than 3 (note that the input will be 1 larger than the actual index)
+    if positionIndex[1].isdigit() and int(positionIndex[1]) <= 3 and int(positionIndex[1]) != 0:
+        positionIndex[1] = int(positionIndex[1]) - 1
+    else:
+        print("\nSorry, the second character needs to be a number between 1 and 3. " +\
+            "Please try again.")
+        return player_input(playerNumber)  
+
+    
+    return positionIndex
+
+print(player_input(1))
